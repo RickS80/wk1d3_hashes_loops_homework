ee1c63764bec91f981e98cd935f4ca1a39e21358
Rick Symington <rick.symington@gmail.com>
2018-11-21 20:07:25 +0000

    Add exercise B

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -55,12 +55,48 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
-# 6. Return an array of Avril's lottery numbers that are even
+e_lottery_numbers = users["Erik"][:lottery_numbers]
+sorted_lottery_numbers = e_lottery_numbers.sort
+p sorted_lottery_numbers[0]
+# 6. Return an array of Avril's lotav_lottery_numbers = []
+for number in users["Avril"][:lottery_numbers]
+  if number % 2 == 0
+    av_lottery_numbers << number
+  end
+end
+p av_lottery_numberstery numbers that are even
+av_lottery_numbers = []
+for number in users["Avril"][:lottery_numbers]
+  if number % 2 == 0
+    av_lottery_numbers << number
+  end
+end
+p av_lottery_numbers
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers] << 7
+p users["Erik"][:lottery_numbers]
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets]<<{:name => "Fluffy", :species => "Dog"}
+p users["Erik"][:pets]
 # 10. Add another person to the users hash
+users["Rick"] =
+  {:twitter => "rick",
+  :lottery_numbers => [1, 2, 3, 4, ],
+  :home_town => "Edinburgh",
+  :pets => [
+    :name => "Taipan",
+    :species => "cat"
+    ]
+}
+p users
--- test.rb
+++ test.rb
@@ -1,3 +1,66 @@
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
 
-p stops[2,1]
+users["Rick"] =
+  {:twitter => "rick",
+  :lottery_numbers => [1, 2, 3, 4, ],
+  :home_town => "Edinburgh",
+  :pets => [
+    :name => "Taipan",
+    :species => "cat"
+    ]
+}
+p users
